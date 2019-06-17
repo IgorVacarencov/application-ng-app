999009fb715100a2a5bebd833fb3fddbf52ae2ac
IgorVacarencov <49471246+IgorVacarencov@users.noreply.github.com>
2019-06-17 10:30:53 +0300

    Basic angular project. The main page with the CRUD functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { UsersComponent } from './users/users.component';
+import { PlacesComponent } from './places/places.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes :Routes = [
+  {
+    path:'users',
+    component: UsersComponent
+  },
+  {
+    path: 'places',
+    component: PlacesComponent
+  },
+  {
+    path:'',
+    component: UsersComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    UsersComponent,
+    PlacesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  model: UserViewModel = {
+    fName:'',
+    lName:'',
+    age:''
+  };
+
+  createUser(): void{
+    let url = "http://localhost:8080/createUser";
+    this.http.post(url, this.model).subscribe(
+      res => {
+        alert("New employee was successfully created! \n Congratulations! ");
+        location.reload();
+      },
+      error => {
+        alert("Ошибка при создании пользователя");
+      }
+    );
+  }
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
+
+export interface UserViewModel {
+fName:string;
+lName:string;
+age:string;
+
+}
